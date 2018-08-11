a74b76e68f054ff9adbeaf52c3da663fa790bd94
Deepak Sharma <deepak.ace12@gmail.com>
2018-08-12 01:28:29 +0530

    removed unwanted code

--- project/static/js/customer.js
+++ project/static/js/customer.js
@@ -2,20 +2,12 @@ var app = angular.module('refearn', []);
 app.controller('refearnController', function($scope, $http){
     $scope.addReferral = function(){
         $http.put('/api/customer/'+$scope.customer+'/?referrer='+$scope.referrer)
-        .then(
-            function(response){
-                window.location.replace("https://www.facebook.com");
-            }, 
-            function(response){
-              // failure callback
-            }
-         );
-        
     }
     $scope.addAmbassodor = function(){
         var doj = new Date($scope.joining_date)
         doc = doj.getFullYear()+"-"+(doj.getMonth()+1)+"-"+doj.getDate()
         var data = {email: $scope.email, joining_date:doc, referral_id:$scope.referral_id, is_ambassador:true}
         $http.post('/api/customer/', data)
+
     }
 })
